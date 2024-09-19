8710f6f2da76c9351dbf7b563b649a59db4bc6a7
isagulov <isa_rus@mail.ru>
2024-09-19 14:41:47 +0300

    Итоговое решение доп задания

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
 sorted_students = sorted(students)
 
-print(sorted_students)
-
 middle_grade = {}
-middle_grade['Aaron'] = sum(grades[0])
-middle_grade['Bilbo'] = sum(grades[1])
-middle_grade['Johny'] = sum(grades[2])
-middle_grade['Khendrik'] = sum(grades[3])
-middle_grade['Steve'] = sum(grades[4])
+middle_grade['Aaron'] = sum(grades[0]) / len(grades[0])
+middle_grade['Bilbo'] = sum(grades[1]) / len(grades[1])
+middle_grade['Johny'] = sum(grades[2]) / len(grades[2])
+middle_grade['Khendrik'] = sum(grades[3]) / len(grades[3])
+middle_grade['Steve'] = sum(grades[4]) / len(grades[4])
 print(middle_grade)
