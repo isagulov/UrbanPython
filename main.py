181870815a7a8007c2a34b738ff66437430bbfe1
isagulov <isa_rus@mail.ru>
2024-09-19 14:37:07 +0300

    First code in Git

---
+++ main.py
@@ -0,0 +1,14 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+sorted_students = sorted(students)
+
+print(sorted_students)
+
+middle_grade = {}
+middle_grade['Aaron'] = sum(grades[0])
+middle_grade['Bilbo'] = sum(grades[1])
+middle_grade['Johny'] = sum(grades[2])
+middle_grade['Khendrik'] = sum(grades[3])
+middle_grade['Steve'] = sum(grades[4])
+print(middle_grade)
