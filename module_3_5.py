3db66a42355b7e4929980b4abb7b4dc12e0fd646
isagulov <74711916+isagulov@users.noreply.github.com>
2024-10-03 11:58:44 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,5 +1,5 @@
 def get_multiplied_digits(number):
-    str_number = str(number)
+    str_number = str(number).rstrip('0')
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
@@ -7,5 +7,5 @@ def get_multiplied_digits(number):
         return first
 
 
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(4020300)
 print(result)
