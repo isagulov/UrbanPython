1a0dded7d7a5a5f43c5982147596938854b8b8ce
isagulov <isa_rus@mail.ru>
2024-10-16 22:20:51 +0300

    Поиск гласных и согласных

---
+++ test.py
@@ -0,0 +1,14 @@
+string = 'Hi! What about you'.lower()
+count_vowel = 0
+count_consonant = 0
+vowels = {'a', 'e', 'i', 'o', 'u'}  # Используем множество для гласных
+
+for i in string:
+    if i.isalpha():  # Проверяем, является ли символ буквой
+        if i in vowels:
+            count_vowel += 1
+        else:
+            count_consonant += 1
+
+print("Гласные:", count_vowel)
+print("Согласные:", count_consonant)
