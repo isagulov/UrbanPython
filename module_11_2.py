c00b848082780f1251ca53fbb2203f941518e2fb
Ruslan <74711916+isagulov@users.noreply.github.com>
2024-12-17 11:33:01 +0300

    Update module_11_2.py
    Интроспекция

--- module_11_2.py
+++ module_11_2.py
@@ -1,7 +1,25 @@
-def introspection_info(obj):
-    obj += 3
-    return obj
+import inspect
 
-print(type(introspection_info))
-print(dir(introspection_info))
-print(introspection_info.__module__)
+
+def introspection_info(obj):
+
+    obj_type = type(obj).__name__
+
+    attributes = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith('__')]
+
+    methods = [method for method in dir(obj) if callable(getattr(obj, method)) and not method.startswith('__')]
+
+    obj_module = inspect.getmodule(obj)
+    module_name = obj_module.__name__ if obj_module else None
+
+    result = {
+        'type': obj_type,
+        'attributes': attributes,
+        'methods': methods,
+        'module': module_name
+    }
+
+    return result
+
+number_info = introspection_info(42)
+print(number_info)
